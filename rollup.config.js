58608f04b47cd94c50be1d9d1f83fb795c136c1c
Fur Frumos <furfrumos@yandex.ru>
2021-07-30 18:45:24 +0300

    welcome build 31

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
-import css from "rollup-plugin-import-css";
+import styles from "rollup-plugin-styles";
 import pkg from "./package.json";
 
 export default {
@@ -8,5 +8,5 @@ export default {
     { file: pkg.main, format: "cjs" },
     { file: pkg.module, format: "es" },
   ],
-  plugins: [typescript(), css()],
+  plugins: [styles(),typescript()],
 };
